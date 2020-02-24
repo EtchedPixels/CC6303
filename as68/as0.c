80c262032552a8453426f9bba49b013381f17784
Alan Cox <alan@linux.intel.com>
2020-02-24 21:06:46 +0000

    as68: Start fleshing out some unixism we need
    - Support cpp # marker parsing
    - Use getopt
    - Allow "-o obj"

--- as68/as0.c
+++ as68/as0.c
@@ -29,79 +29,97 @@ int	debug_write = 1 ;
 int	noobj;
 int	cpu_flags = ARCH_CPUFLAGS;
 
+static void usage(void)
+{
+	fprintf(stderr, "as [-o object.o] source.s.\n");
+	exit(1);
+}
+
+static void oom(void)
+{
+	fprintf(stderr, "Out of memory.\n");
+	exit(1);
+}
+
+static char *xstrdup(const char *p)
+{
+	char *n = strdup(p);
+	if (!n)
+		oom();
+	return n;
+}
+
 int main(int argc, char *argv[])
 {
 	char *ifn;
-	char *ofn;
-	char *p;
+	char *ofn = NULL;
+	char *p, *e;
 	int i;
 	int c;
 
-	/* FIXME: switch to getopt - add map, listing */
-	ifn = NULL;
-	for (i=1; i<argc; ++i) {
-		p = argv[i];
-		if (*p == '-') {
-			while ((c = *++p) != 0) {
-				switch (c) {
-				/* FIXME: move this to z80 .setcpu */
-				case '1':
-					cpu_flags |= OA_8080_Z180;
-					break;
-				default:
-					fprintf(stderr, "Bad option %c\n", c);
-					exit(BAD);
-				}
-			}
-		} else if (ifn == NULL)
-			ifn = p;
-		else {
-			fprintf(stderr, "Too many source files\n");
-			exit(BAD);
+	int opt;
+
+	/* Lots of options need adding yet */
+	while ((opt = getopt(argc, argv, "o:")) != -1) {
+		switch (opt) {
+		case 'o':
+			ofn = optarg;
+			break;
+		default:
+			usage();
+			break;
 		}
 	}
-	if (ifn == NULL) {
-		fprintf(stderr, "No source file\n");
-		exit(BAD);
-	}
+	if (optind != argc - 1)
+		usage();
+	ifn = argv[optind];
 
 	if ((ifp=fopen(ifn, "r")) == NULL) {
 		fprintf(stderr, "%s: cannot open\n", ifn);
 		exit(BAD);
 	}
 
-	ofn = strdup(ifn);
 	if (ofn == NULL) {
-		fprintf(stderr, "%s: out of memory.\n", ifn);
-		exit(BAD);
+		ofn = xstrdup(ifn);
+		p = strrchr(ofn, '.');
+		if (p == NULL || p[1] == 0) {
+			fprintf(stderr, "%s: expected extensions.\n", ifn);
+			exit(BAD);
+		}
+		p[1] = 'o';
+		p[2] = '\0';
 	}
-	p = strrchr(ofn, '.');
-	if (p == NULL || p[1] == 0) {
-		fprintf(stderr, "%s: expected extensions.\n", ifn);
-		exit(BAD);
-	}
-
-	p[1] = 'o';
-	p[2] = '\0';
 
 	if ((ofp=fopen(ofn, "w")) == NULL) {
 		fprintf(stderr, "%s: cannot create.\n", ofn);
 		exit(BAD);
 	}
 	syminit();
-	fname = ifn;
+	fname = xstrdup(ifn);
 	for (pass=0; pass<4; ++pass) {
 		/* FIXME: for some platforms we should just do 0 and 3 */
 		outpass();
-		line = 0;
+		line = 1;
 		memset(dot, 0, sizeof(dot));
 		fseek(ifp, 0L, 0);
 		while (fgets(ib, NINPUT, ifp) != NULL) {
-			++line;
-			ep = &eb[0];
-			ip = &ib[0];
-			if (setjmp(env) == 0)
-				asmline();
+			/* Pre-processor output */
+			if (*ib == '#' && ib[1] == ' ') {
+				free(fname);
+				line = strtoul(ib +2, &p, 10);
+				p++;
+				e = strrchr(p , '"');
+				if (e)
+					*e = 0;
+				fname = xstrdup(p);
+			/* Normal assembly */
+			} else {
+				ep = &eb[0];
+				ip = &ib[0];
+				if (setjmp(env) == 0)
+					asmline();
+				++line;
+			}
 		}
 		/* Don't continue once we know we failed */
 		if (noobj)
