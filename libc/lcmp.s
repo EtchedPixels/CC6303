65738771921c04669d667cfb9e91268103ee21c7
Alan Cox <alan@linux.intel.com>
2020-03-01 20:49:01 +0000

    lcmp: do subtraction the right way around

--- libc/lcmp.s
+++ libc/lcmp.s
@@ -9,10 +9,11 @@
 toslcmp:
 	tsx
 	std @tmp	; Save the low 16bits
-	ldd @sreg
-	subd 5,x	; Compare the high word
+	ldd 5,x
+	subd @sreg	; Check the high word
 	beq chklow
-chklow:			; High is same so compare low
-	ldd @tmp
-	subd 7,x
+	rts
+chklow:
+	ldd 7,x		; High is same so compare low
+	subd @tmp
 	rts
