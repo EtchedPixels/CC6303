5a099bd961b2ad398a4a2d17028a81538b61df20
Alan Cox <alan@linux.intel.com>
2020-04-27 20:46:26 +0100

    lib6800: tosmuleax

--- lib6800/tosumuleax.s
+++ lib6800/tosumuleax.s
@@ -1,10 +1,12 @@
 ;
 ;	Top of stack (1,x) * sreg:d
 ;
+		.export tosmuleax
 		.export tosumuleax
 
 		.code
 tosumuleax:
+tosmuleax:
 		tsx
 		jsr umul32x32	; does the actual work
 		jmp pop4	; and discard the argument
