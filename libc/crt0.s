b0802f1befe33e356f5ab2fe3774283d5bd36dca
Alan Cox <alan@linux.intel.com>
2020-05-01 23:17:19 +0100

    crt0: add exit

--- libc/crt0.s
+++ libc/crt0.s
@@ -3,19 +3,32 @@
 ;
 	.code
 
+	.export _exit
+
 start:
 	ldaa #1
 	staa @one
 	clra
 	clrb
+	sts exitsp
 	psha
 	pshb
 	psha
 	pshb
+	ldab #4
 	jsr _main
-	ins
-	ins
-	ins
-	ins
+	bra doexit
+
+_exit:
+	tsx
+	ldaa 3,x
+	ldab 4,x
+doexit:
+;
+;	No atexit support yet
+;
+	lds exitsp
 	rts
 
+	.bss
+exitsp:
