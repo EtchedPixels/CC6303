49b63c0dc19d56154cebdd3a4a5e96e9a372b4e8
Alan Cox <alan@linux.intel.com>
2021-05-22 16:50:11 +0100

    lib6803: fix 16bit maths for reg v top of stack
    Hopefully takes us a bit closer to integer behaving right

--- lib6803/tosumulax.s
+++ lib6803/tosumulax.s
@@ -20,12 +20,12 @@ tosumulax:
 	pulb
 	mul			; D is now high x low
 	std @tmp1
-	ldaa 1,x		; low byte
+	ldaa 3,x		; low byte
 	pulb			; high byte of D
 	mul			; D is now low x high
 	addd @tmp1		; High bytes
 	tba			; Shift left 8, discarding
-	clra
+	clrb
 	addd @tmp		; Add the low x low
 	jmp pop2
 
