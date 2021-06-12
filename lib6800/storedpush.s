1973e882f8d6286aa3d775ce8ac7139cb1aca225
Alan Cox <alan@linux.intel.com>
2021-06-12 16:05:41 +0100

    6800: fix the 6800 specific storedpush helper

--- lib6800/storedpush.s
+++ lib6800/storedpush.s
@@ -9,11 +9,10 @@ storedpush:
 	staa 2,x		; save D at top of stack (as seen by caller)
 	stab 3,x
 pshtop:
+	tsx
 	ldx ,x			; get return address
 	stx @tmp		; patch it in
 	tsx
 	ldx 2,x			; get saved D
 	stx ,x			; overwrite return address
 	jmp jmptmp		; to caller without adjusting stack
-
-
