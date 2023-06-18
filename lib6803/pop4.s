069be789719aac001d85c01fa6028101a9ebbd3f
Alan Cox <etchedpixels@gmail.com>
2023-06-18 12:44:38 +0100

    6803: restore pop4flags

--- lib6803/pop4.s
+++ lib6803/pop4.s
@@ -4,6 +4,7 @@
 	.export pop4
 	.export tpop4
 	.export swap32pop4
+	.export pop4flags
 
 	.setcpu 6803
 	.code
@@ -14,8 +15,14 @@ tpop4:
 	ldd @tmp
 pop4:
 	pulx
+pop4h:
 	ins
 	ins
 	ins
 	ins
 	jmp ,x
+
+pop4flags:
+	pulx
+	tstb
+	bra pop4h
