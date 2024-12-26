8cce730a1a316896c0b737d929b20a1afbfab270
zu2 <3023910+zu2@users.noreply.github.com>
2024-12-26 18:20:31 +0900

    add missing library.

---
+++ lib6800/tosshreax.s
@@ -0,0 +1,45 @@
+;
+;	Right shift 32bit signed
+;
+	.export tosshreax
+
+	.code
+
+tosshreax:	
+	cmpb	#32
+	bcc	ret0
+	tstb
+	beq noshift
+	tsx
+loop:
+	lsr	2,x
+	ror	3,x
+	ror	4,x
+	ror	5,x
+	decb
+	bne loop
+	ldaa	2,x
+	ldab	3,x
+	; Get the value
+	staa	@sreg
+	stab	@sreg+1
+	ldaa	4,x
+	ldab	5,x
+	jmp pop4
+ret0:
+	clra
+	clrb
+	staa	@sreg
+	stab	@sreg+1
+	bra	noshift
+noshift:
+	ldx	0,x
+	ins
+	ins
+	pula
+	staa	@sreg
+	pulb
+	stab	@sreg+1
+	pula
+	pulb
+	jmp	0,x
